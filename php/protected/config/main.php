89bdb48a3cdcce287d40eb9dc18d70d36a1a9765
Dave <davey.glenn@gmail.com>
2013-08-09 22:01:49 +0100

    connection info changed-class in main.php

--- php/protected/config/main.php
+++ php/protected/config/main.php
@@ -51,7 +51,7 @@ return array(
 		),
 		*/
 		'mongodb' => array(
-			'class' => 'EMongoClient',
+			//'class' => 'EMongoClient',
 			//'server' => 'mongodb://localhost:27017',
 			'server' => 'mongodb://admin:3UtiRg4ZdhYq@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/',
 			'db' => 'one'
