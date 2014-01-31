3e2259a36164df1da1d97b9e2e3c60eb52e235c0
OpenShift guest <davey.glenn@gmail.com>
2014-01-31 08:49:21 -0500

    first change on cloud9

--- php/protected/views/site/index.php
+++ php/protected/views/site/index.php
@@ -6,7 +6,7 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Congratulations! You have successfully created your Yii application. And this is hosted on the openshift platform and the files have been edited on the cloud 9 ide</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
